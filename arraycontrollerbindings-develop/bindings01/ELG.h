8313a8b51b516e4e7a38d258cc82a74ec0ac2798
yueli <>
2019-04-17 21:27:04 +0800

    no message

---
+++ arraycontrollerbindings-develop/bindings01/ELG.h
@@ -0,0 +1,17 @@
+//
+//  ELG.h
+//  bindings01
+//
+//  Created by ganping on 2019/4/17.
+//  Copyright © 2019年 Eric Gorr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ELG : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ arraycontrollerbindings-develop/bindings01/ELG.m
@@ -0,0 +1,13 @@
+//
+//  ELG.m
+//  bindings01
+//
+//  Created by ganping on 2019/4/17.
+//  Copyright © 2019年 Eric Gorr. All rights reserved.
+//
+
+#import "ELG.h"
+
+@implementation ELG
+
+@end
